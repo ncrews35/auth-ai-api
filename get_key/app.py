98fa9fd15237c73bd008d1230306799a7afeb66d
Nick Crews <nickcrews35@gmail.com>
2023-06-06 23:21:30 -0400

    Finalize get key api

--- get_key/app.py
+++ get_key/app.py
@@ -4,18 +4,35 @@ from db import DBTable
 
 
 def get_key(event, context):
-    print(event)
-    print(context)
-    user_id = event.get('user')
-    provider_id = event.get('provider')
+    try:
+        provider_id = event["queryStringParameters"].get('provider')
+        user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Missing required parameters'
+            })
+        }
+
+    if not provider_id:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Missing required parameters'
+            })
+        }
 
     keys_table = DBTable(os.getenv('KEYS_TABLE'))
     key = keys_table.get_sort('user_id', user_id, 'provider_id', provider_id)
 
-    if key:
+    provider_id = key.get('provider_id')
+    value = key.get('value')
+
+    if provider_id and value:
         return {
             'statusCode': 200,
-            'body': json.dumps(key)
+            'body': json.dumps({provider_id: value})
         }
     else:
         return {
